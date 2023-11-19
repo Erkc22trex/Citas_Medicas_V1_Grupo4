9a19537a327a2bf168af49d939d16d04be319e61
Erick Trejo <>
2023-11-19 11:48:49 -0600

    modificacion vista persona

--- php/ControladorPersona.php
+++ php/ControladorPersona.php
@@ -1,26 +1,64 @@
 <?php
 include 'DAOPersona.php';
 
+// Verificar si se recibió una solicitud POST desde el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Crear un objeto Persona con los datos del formulario
-    $persona = new Persona();
-    $persona->setPrimerNombre($_POST["primerNombre"]);
-    $persona->setSegundoNombre($_POST["segundoNombre"]);
-    $persona->setPrimerApellido($_POST["primerApellido"]);
-    $persona->setSegundoApellido($_POST["segundoApellido"]);
-    $persona->setDni($_POST["dni"]);
-    $persona->setTelefono($_POST["telefono"]);
-    $persona->setSexo($_POST["sexo"]);
-    $persona->setFechaDeNacimiento($_POST["fechaDeNacimiento"]);
-    $persona->setEdad($_POST["edad"]);
-    $persona->setDireccion($_POST["direccion"]);
-    $persona->setCorreoElectronico($_POST["correoElectronico"]);
-
-    // Instanciar y utilizar DAOPersona para ingresar la persona
+    // Instanciar y utilizar DAOPersona para manejar la acción solicitada
     $daoPersona = new DAOPersona();
-    $resultado = $daoPersona->ingresarPersona($persona);
 
-    // Verificar el resultado de la inserción
-    echo $resultado;
+    // Verificar la acción solicitada
+    if ($_POST["accion"] === "Ver Persona") {
+        // Código para ver persona según requerimientos
+        $dni = $_POST["dni"];
+        $resultado = $daoPersona->verPersona($dni);
+
+        if (is_array($resultado)) {
+            // Mostrar los datos en una tabla o como desees
+            echo "<table>";
+            foreach ($resultado as $campo => $valor) {
+                echo "<tr><td>$campo</td><td>$valor</td></tr>";
+            }
+            echo "</table>";
+        } else {
+            echo $resultado; // Mostrar mensaje de error si la persona no se encuentra
+        }
+    } elseif ($_POST["accion"] === "Actualizar Persona") {
+        // Código para actualizar persona según requerimientos
+    } elseif ($_POST["accion"] === "Eliminar Persona") {
+        // Código para eliminar persona según requerimientos
+    } elseif ($_POST["accion"] === "Insertar Persona") {
+        // Crear un objeto Persona con los datos del formulario
+        $persona = new Persona();
+        $persona->setPrimerNombre($_POST["primerNombre"]);
+        $persona->setSegundoNombre($_POST["segundoNombre"]);
+        $persona->setPrimerApellido($_POST["primerApellido"]);
+        $persona->setSegundoApellido($_POST["segundoApellido"]);
+        $persona->setDni($_POST["dni"]);
+        $persona->setTelefono($_POST["telefono"]);
+        $persona->setSexo($_POST["sexo"]);
+        $persona->setFechaDeNacimiento($_POST["fechaDeNacimiento"]);
+        $persona->setEdad($_POST["edad"]);
+        $persona->setDireccion($_POST["direccion"]);
+        $persona->setCorreoElectronico($_POST["correoElectronico"]);
+
+        // Llamar a la función para ingresar persona en DAOPersona
+        $resultado = $daoPersona->ingresarPersona($persona);
+        echo $resultado;
+    }
 }
-?>
+
+// Verificar si se recibió una solicitud GET desde el formulario de búsqueda por DNI
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // Obtener el DNI ingresado para la búsqueda
+    $dni = $_GET["dni"];
+
+    // Instanciar y utilizar DAOPersona para buscar la persona por DNI
+    $daoPersona = new DAOPersona();
+    $resultadoBusqueda = $daoPersona->verPersona($dni);
+
+    // Mostrar el resultado de la búsqueda en la tabla de resultados del formulario
+    // ...
+}
+
+
+
--- php/DAOpersona.php
+++ php/DAOpersona.php
@@ -1,11 +1,14 @@
 <?php
 include 'Persona.php';
 include 'informacion.php';
-class DAOpersona {
-   private $conn;
+
+class DAOPersona {
+    private $conn;
+
     public function __construct() {
         $this->conectar();
-        }
+    }
+
     public function conectar() {
         $this->conn = new mysqli(SERVIDOR, USUARIO, CLAVES, BD);
 
@@ -13,88 +16,56 @@ class DAOpersona {
             die("Error de conexión: " . $this->conn->connect_error);
         }
     }
-        //desconectar de la base de datos
+
     public function desconectar() {
-        $this->conn->close;}
-        
-        public function getTabla() {
-    $sql = "SELECT * FROM persona"; // Consulta SQL para obtener todas las personas
-    $this->conectar(); // Conexión a la base de datos
-    $res = $this->conn->query($sql); // Almacenar el resultado de la consulta
-
-    // Creación de la tabla HTML
-    $tabla = "<table class='table table-dark'>
-                <thead class='thead thead-light'>
-                    <tr>
-                        <th>Primer Nombre</th>
-                        <th>Segundo Nombre</th>
-                        <th>Primer Apellido</th>
-                        <th>Segundo Apellido</th>
-                        <th>DNI</th>
-                        <th>Telefono</th>
-                        <th>Sexo</th>
-                        <th>Fecha de Nacimiento</th>
-                        <th>Edad</th>
-                        <th>Direccion</th>
-                        <th>Correo Electrónico</th>
-                        <!-- Agrega más columnas si es necesario para mostrar la información -->
-                        <th>Acción</th>
-                    </tr>
-                </thead>
-                <tbody>";
-
-    // Construcción del cuerpo de la tabla con los datos de la base de datos
-    while ($tupla = mysqli_fetch_assoc($res)) {
-        $tabla .= "<tr>
-                    <td>".$tupla["Primer_Nombre"]."</td>
-                    <td>".$tupla["Segundo_Nombre"]."</td>
-                    <td>".$tupla["Primer_Apellido"]."</td>
-                    <td>".$tupla["Segundo_Apellido"]."</td>
-                    <td>".$tupla["Dni"]."</td>
-                    <td>".$tupla["Telefono"]."</td>
-                    <td>".$tupla["Sexo"]."</td>
-                    <td>".$tupla["Fecha_de_nacimiento"]."</td>
-                    <td>".$tupla["Edad"]."</td>
-                    <td>".$tupla["Direccion"]."</td>
-                    <td>".$tupla["Correo_Electronico"]."</td>
-                    <!-- Agrega más celdas si hay más columnas -->
-                    <td><a href=\"actualizar.php?dni=".$tupla["Dni"]."\">Actualizar</a> | 
-                    <a href=\"eliminar.php?dni=".$tupla["Dni"]."\">Eliminar</a></td>
-                </tr>";
+        $this->conn->close();
     }
 
-    $tabla .= "</tbody></table>"; // Cierre de la tabla
-    $res->close(); // Cierre del resultado
-    $this->desconectar(); // Cierre de la conexión
-    return $tabla; // Retorno de la tabla HTML
-}
-    public function existe($objeto) {
-    $p = $objeto;
-    $sql = "SELECT * FROM persona WHERE Dni = ?";
-    $stmt = $this->conn->prepare($sql);
+    public function getTabla() {
+        $sql = "SELECT * FROM persona";
+        $this->conectar();
+        $res = $this->conn->query($sql);
 
-    if ($stmt) {
-        $dni = $p->getDni();
-        $stmt->bind_param("s", $dni);
-        $stmt->execute();
-        $result = $stmt->get_result();
+        $tabla = "<table class='table table-dark'>
+                    <!-- Encabezados de la tabla -->
+                    <!-- ... -->
+                    <tbody>";
 
-        if ($result->num_rows > 0) {
-            // La persona existe
-            $stmt->close();
-            return true;
-        } else {
-            // La persona no existe
-            $stmt->close();
-            return false;
+        while ($tupla = mysqli_fetch_assoc($res)) {
+            // Construcción de filas con datos de la base de datos
+            // ...
         }
-    } else {
-        // Error en la preparación de la consulta
-        return "Hubo un error en la preparación de la consulta.";
-    }
-}
 
-    public function ingresarPersona($objeto) {
+        $tabla .= "</tbody></table>";
+        $res->close();
+        $this->desconectar();
+        return $tabla;
+    }
+
+    public function existe($objeto) {
+        $p = $objeto;
+        $sql = "SELECT * FROM persona WHERE Dni = ?";
+        $stmt = $this->conn->prepare($sql);
+
+        if ($stmt) {
+            $dni = $p->getDni();
+            $stmt->bind_param("s", $dni);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows > 0) {
+                $stmt->close();
+                return true;
+            } else {
+                $stmt->close();
+                return false;
+            }
+        } else {
+            return "Hubo un error en la preparación de la consulta.";
+        }
+    }
+
+        public function ingresarPersona($objeto) {
     $p = $objeto;
 
     // Verificar si la persona ya existe antes de insertarla
@@ -149,44 +120,33 @@ class DAOpersona {
     }
 }
 
-    // Método para ver información de una persona por su DNI
     public function verPersona($dni) {
-    $this->conn;
-
-    // Verificar si la persona existe antes de consultar sus datos
-    if (!$this->existePersonaByDNI($dni)) {
-        return "La persona con DNI $dni no existe.";
-    }
-
-    // Consulta para obtener información de una persona por su DNI
     $sql = "SELECT * FROM persona WHERE Dni=?";
     $stmt = $this->conn->prepare($sql);
-    $stmt->bind_param("s", $dni);
-    $stmt->execute();
-    $result = $stmt->get_result();
 
-    if ($result->num_rows > 0) {
-        // La persona existe, obtener y devolver la información
-        $personaInfo = $result->fetch_assoc();
-        return $personaInfo; // Aquí podrías hacer lo que desees con esta información
+    if ($stmt) {
+        $stmt->bind_param("s", $dni);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $personaInfo = $result->fetch_assoc();
+            $stmt->close();
+            return $personaInfo;
+        } else {
+            $stmt->close();
+            return "La persona con DNI $dni no existe.";
+        }
     } else {
-        return "La persona con DNI $dni no existe.";
+        return "Hubo un error en la preparación de la consulta.";
     }
 }
 
-    // Método para actualizar información de una persona por su DNI
-    public function actualizarPersona(Persona $persona) {
-    $this->conn;
-
-    // Preparar la consulta SQL para actualizar una persona por su DNI
-    $sql = "UPDATE tblpersona SET primerNombre=?, segundoNombre=?, primerApellido=?, segundoApellido=?, dni=?, telefono=?, sexo=?, fechaDeNacimiento=?, Edad=?, direccion=?, correoElectronico=? WHERE dni=?";
+public function actualizarPersona(Persona $persona) {
+    $sql = "UPDATE persona SET Primer_Nombre=?, Segundo_Nombre=?, Primer_Apellido=?, Segundo_Apellido=?, Telefono=?, Sexo=?, Fecha_de_nacimiento=?, Edad=?, Direccion=?, Correo_Electronico=? WHERE Dni=?";
     $stmt = $this->conn->prepare($sql);
 
-    // Verificar si la preparación de la consulta fue exitosa
     if ($stmt) {
-        // Obtener los datos de la persona para actualizar
-        $dni = $persona->getDni();
-        $idPersona = $persona->getDni();
         $primerNombre = $persona->getPrimerNombre();
         $segundoNombre = $persona->getSegundoNombre();
         $primerApellido = $persona->getPrimerApellido();
@@ -194,28 +154,25 @@ class DAOpersona {
         $telefono = $persona->getTelefono();
         $sexo = $persona->getSexo();
         $fechaDeNacimiento = $persona->getFechaDeNacimiento();
-        $Edad = $persona->getEdad();
+        $edad = $persona->getEdad();
         $direccion = $persona->getDireccion();
         $correoElectronico = $persona->getCorreoElectronico();
+        $dni = $persona->getDni();
 
-        // Enlazar parámetros
-        $stmt->bind_param("issssssssiss", 
-            $idPersona,
+        $stmt->bind_param("ssssisssiss",
             $primerNombre,
             $segundoNombre,
             $primerApellido,
             $segundoApellido,
-            $dni,
             $telefono,
             $sexo,
             $fechaDeNacimiento,
-            $Edad,
+            $edad,
             $direccion,
             $correoElectronico,
-            $dni // Usamos el DNI como referencia para la actualización
+            $dni
         );
 
-        // Ejecutar la consulta
         if ($stmt->execute()) {
             return "Persona actualizada correctamente.";
         } else {
@@ -226,28 +183,20 @@ class DAOpersona {
     }
 }
 
+public function eliminarPersona($dni) {
+    $sql = "DELETE FROM persona WHERE Dni=?";
+    $stmt = $this->conn->prepare($sql);
 
-    // Método para eliminar una persona por su DNI
-    public function eliminarPersona($dni) {
-        $this->conn;
+    if ($stmt) {
+        $stmt->bind_param("s", $dni);
 
-        // Preparar la consulta SQL para eliminar una persona por su DNI
-        $sql = "DELETE FROM tblpersona WHERE dni=?";
-        $stmt = $this->conn->prepare($sql);
-
-        // Verificar si la preparación de la consulta fue exitosa
-        if ($stmt) {
-            // Enlazar parámetros
-            $stmt->bind_param("s", $dni);
-
-            // Ejecutar la consulta
-            if ($stmt->execute()) {
-                return "Persona eliminada correctamente.";
-            } else {
-                return "Hubo un problema al eliminar la persona.";
-            }
+        if ($stmt->execute()) {
+            return "Persona eliminada correctamente.";
         } else {
-            return "Hubo un error en la preparación de la consulta.";
+            return "Hubo un problema al eliminar la persona.";
         }
+    } else {
+        return "Hubo un error en la preparación de la consulta.";
     }
-    }
+}
+}
