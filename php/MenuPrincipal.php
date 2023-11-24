29055ee96c8ea22b045d962a5a82baf38d3f1841
Erick Trejo <user@DESKTOP-9V65GE2.mshome.net>
2023-11-23 22:46:17 -0600

    ESTE ES UN CAMBIO

--- php/MenuPrincipal.php
+++ php/MenuPrincipal.php
@@ -7,58 +7,67 @@
   <style>
     /* Estilos adicionales personalizados */
     body {
-      padding: 20px;
+      padding-bottom: 60px; /* Agregamos un espacio en la parte inferior para los botones */
       font-family: Arial, sans-serif;
     }
 
     h1 {
       text-align: center;
+      margin-top: 20px;
     }
 
     /* Estilo para los botones */
     .menu-button {
       margin: 5px;
     }
+
+    /* Estilo para el footer */
+    .footer-buttons {
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      background-color: #f8f9fa; /* Color de fondo */
+      padding: 10px 0; /* Espaciado interno */
+      text-align: center; /* Centrar el contenido */
+    }
   </style>
 </head>
 <body>
   <h1>Bienvenido al Sistema de Gestión Clínica</h1>
-  
+
+  <!-- Contenedor de los botones -->
   <div class="d-grid gap-2">
     <!-- Botones con clases de Bootstrap -->
-    <button class="btn btn-primary menu-button" onclick="vistaPersona()">PACIENTE</button>
-    <button class="btn btn-secondary menu-button" onclick="VistaPaciente()"></button>
-    <button class="btn btn-success menu-button" onclick="VistaMedico()">CRUD MEDICO</button>
-    <!-- <button class="btn btn-danger menu-button" onclick="eliminarPaciente()">Eliminar Paciente</button> -->
+    <button class="btn btn-primary btn-sm menu-button" onclick="vistaPersona()">CRUD PACIENTE</button>
+    <button class="btn btn-secondary btn-sm menu-button" onclick="VistaPaciente()">CRUD ADMINISTRADOR</button>
+    <button class="btn btn-success btn-sm menu-button" onclick="VistaMedico()">CRUD MEDICO</button>
+    <!-- <button class="btn btn-danger btn-sm menu-button" onclick="eliminarPaciente()">Eliminar Paciente</button> -->
+  </div>
+
+  <!-- Footer para los botones -->
+  <div class="footer-buttons">
+    <!-- Coloca los botones en la parte inferior -->
+    <button class="btn btn-primary btn-sm menu-button" onclick="vistaPersona()">CRUD PACIENTE</button>
+    <button class="btn btn-secondary btn-sm menu-button" onclick="VistaPaciente()">CRUD ADMINISTRADOR</button>
+    <button class="btn btn-success btn-sm menu-button" onclick="VistaMedico()">CRUD MEDICO</button>
+    <!-- <button class="btn btn-danger btn-sm menu-button" onclick="eliminarPaciente()">Eliminar Paciente</button> -->
   </div>
 
   <!-- Scripts -->
-
   <script>
     function vistaPersona() {
-      // Aquí podrías redirigir a la página de ingreso de un paciente
-      // Por ejemplo: window.location.href = 'ingresar_paciente.html';
       window.location.href = 'vistaPersona.php';
     }
 
-    function buscarPaciente() {
-      // Aquí podrías redirigir a la página de búsqueda de pacientes
-      // Por ejemplo: window.location.href = 'buscar_paciente.html';
-      console.log('Redirigir a la página de búsqueda de pacientes');
-    }
-
-    function actualizarPaciente() {
-      // Aquí podrías redirigir a la página de actualización de pacientes
-      // Por ejemplo: window.location.href = 'actualizar_paciente.html';
-      console.log('Redirigir a la página de actualización de pacientes');
-    }
-
-    function eliminarPaciente() {
-      // Aquí podrías redirigir a la página de eliminación de pacientes
-      // Por ejemplo: window.location.href = 'eliminar_paciente.html';
-      console.log('Redirigir a la página de eliminación de pacientes');
-    }
-  </script>
+//    function buscarPaciente() {
+//      console.log('Redirigir a la página de búsqueda de pacientes');
+//    }
+//
+//    function actualizarPaciente() {
+//      console.log('Redirigir a la página de actualización de pacientes');
+//    }
+//
+//    function eliminarPaciente
+</SCRIPT>
 </body>
 </html>
-
