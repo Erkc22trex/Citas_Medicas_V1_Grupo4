ee44fca5c67d1ef2f91835e6fb3a9ffa217490f1
joelv <joelv@DESKTOP-9AT2FLB>
2023-11-23 22:44:46 -0600

    cambios 1

--- php/pacientes/Paciente.php
+++ php/pacientes/Paciente.php
@@ -1,85 +1,34 @@
 <?php
 
-
-class Paciente {
+class Paciente extends Persona {
     //atributos
-    private $conn;
-    private $id;
-    private $dni;
-    private $nombre;
-    private $telefono;
-    private $citas;
-    private $sexo;
-    private $fch_nac;
-    private $correo;
+    // private $conn;
+    // private $dni;
+    // private $nombre;
+    // private $telefono;
+    // private $citas;
+    // private $sexo;
+    // private $fch_nac;
+    // private $correo;
+    private $id_paciente;
+
+    public function __construct($id, $nombre, $apellido, $fechaNacimiento, $otrosDatos, $rol, $id_paciente) {
+        parent::__construct($id, $nombre, $apellido, $fechaNacimiento, $otrosDatos, $rol);
+        $this->id_paciente = $id_paciente;
+    }
     
     //constructor
-    public function __construct() {
+    public function construct() {
         
     }
+    
     // Getters y setters 
     public function getIdPaciente() {
         return $this->id;
     }
-    
-    public function getDni() {
-        return $this->dni;
-    }
 
-    public function getNombre() {
-        return $this->nombre;
-    }
-
-    public function getTelefono() {
-        return $this->telefono;
-    }
-    
-        public function getCitas() {
-        return $this->citas;
-    }
-
-    public function getSexo() {
-        return $this->sexo;
-    }
-
-    public function getFchNac() {
-        return $this->fch_nac;
-    }
-
-    public function getCorreo() {
-        return $this->correo;
-    }
-
-    public function setIdPersona($id){
+    public function setIdPaciente($id){
         $this->id = $id;
-    }
-    
-    public function setDni($dni){
-        $this->dni = $dni;
-    }
-
-    public function setNombre($nombre){
-        $this->nombre = $nombre;
-    }
-
-    public function setTelefono($telefono){
-        $this->telefono = $telefono;
-    }
-    
-        public function setCitas($citas){
-        $this->citas = $citas;
-    }
-
-    public function setSexo($sexo){
-        $this->sexo = $sexo;
-    }
-
-    public function setFchNac($fchNac){
-        $this->fch_nac = $fchNac;
-    }
-
-    public function setCorreo($correo){
-        $this->correo = $correo;
     }
 
 }
