87176d1237e21851d6041909d29bb404a865e303
joelv <joelv@DESKTOP-9AT2FLB>
2023-11-25 23:34:35 -0600

    ultimos estilos

--- php/medicos/TablaMedicos.php
+++ php/medicos/TablaMedicos.php
@@ -11,11 +11,12 @@ $med = new Medico();
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../styles/styles.css">
 </head>
 
 <body>
     <div class="px-5 py-2">
-        <button type="submit" id="btnAgregar" name="btnAgregar" class="btn btn-primary" onclick="return regresar()">Inicio</button>
+        <button type="submit" id="btnAgregar" name="btnAgregar" class="btn btn-primary custon-btn" onclick="return regresar()">Inicio</button>
     </div>
     <section class="d-flex flex-column justify-content-center align-items-center">
         <h2 class="text-center">Medicos</h2>
@@ -52,7 +53,7 @@ $med = new Medico();
         </form>
     </section>
     <div class="d-flex justify-content-end align-items-center me-5 pe-2 mb-3">
-        <button type="submit" id="btnAgregarPac" name="btnAgregarPac" class="btn btn-success" onclick="return agregar('agregar')">Agregar medicos</button>
+        <button type="submit" id="btnAgregarPac" name="btnAgregarPac" class="btn btn-success custon-btn" onclick="return agregar('agregar')">Agregar medicos</button>
     </div>
     <?php
     $foot = "<section class='p-5 overflow-auto' style='max-height: 500px;'>";
